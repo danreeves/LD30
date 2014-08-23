ef191bb21e355d865dafd520e74cff459803bc7b
Dan Reeves <heydanreeves@gmail.com>
2014-08-23 11:48:31 +0100

    readme, and tilemaps WOOOO

---
+++ README.md
@@ -0,0 +1,7 @@
+
+
+## Assets
+
+ - http://opengameart.org/content/space-soldier-m484-games
+ - http://opengameart.org/content/sci-fi-platform-tiles
+ - http://opengameart.org/content/sci-fi-platform-tiles-sectioned
--- game.js
+++ game.js
@@ -5,19 +5,23 @@ var mainState = {
     preload: function() {
         game.stage.backgroundColor = 0x4488cc;
         game.load.spritesheet('player', 'img/player.gif', 50, 50, 9);
+        game.load.tilemap('map', 'maps/1.json', null, Phaser.Tilemap.TILED_JSON);
+        game.load.image('level', 'img/tilemap.png');
     },
     create: function() {
-        // Definitions
-        this.p1 = this.game.add.sprite(100, 245, 'player');
-        this.cursor = this.game.input.keyboard.createCursorKeys();
-        this.spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-
         // World
         game.physics.startSystem(Phaser.Physics.ARCADE);
-        game.physics.arcade.enable(this.p1);
-        game.camera.follow(this.p1, Phaser.Camera.FOLLOW_PLATFORMER);
+        this.map = game.add.tilemap('map');
+        this.map.addTilesetImage('level');
+        this.layer = this.map.createLayer('Tile Layer 1');
+        this.map.setCollisionBetween(0, 100);
+        this.layer.resizeWorld();
+        // layer.debug = true;
 
         // Player
+        this.p1 = this.game.add.sprite(0, this.game.world.height-64, 'player');
+        game.physics.arcade.enable(this.p1);
+        game.camera.follow(this.p1, Phaser.Camera.FOLLOW_PLATFORMER);
         this.p1.body.gravity.y = 1000;
         this.p1.body.maxVelocity.setTo(200,9999);
         this.p1.body.drag.setTo(1000,-100);
@@ -25,12 +29,15 @@ var mainState = {
         this.p1.anchor.setTo(0.5, 0.5);
 
         // Controls
+        this.cursor = this.game.input.keyboard.createCursorKeys();
+        this.spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         this.cursor.up.onDown.add(this.jump, this);
 
         // Animations
         this.p1.animations.add('walk',[1,2,3,4,5,6,7,8,9]);
     },
     update: function() {
+        this.physics.arcade.collide(this.p1, this.layer);
         this.move();
     },
 
