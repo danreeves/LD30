e3e1da7a02d0666ac3fbd1420e83d4bcd3c82fba
Dan Reeves <heydanreeves@gmail.com>
2014-08-23 09:17:59 +0100

    full page canvas

---
+++ game.js
@@ -0,0 +1,12 @@
+var game = new Phaser.Game(screen.width, screen.height, Phaser.CANVAS, 'game', { preload: preload, create: create, render: render });
+
+
+function preload () {
+
+}
+function create() {
+    this.game.stage.backgroundColor = 0x4488cc;
+}
+function render () {
+
+}
