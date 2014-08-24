a99b6b983a1dbca71691bec5f256dca4fdeab3d4
Dan Reeves <heydanreeves@gmail.com>
2014-08-24 01:55:37 +0100

    demons that OMINOUSLY WALK TOWARDS YOU! 👹

--- game.js
+++ game.js
@@ -23,18 +23,35 @@ var mainState = {
         this.map.setCollisionBetween(0, 1000);
         this.layer.resizeWorld();
 
-        // layer.debug = true;
+        game.physics.arcade.gravity.y = 1000;
+        // this.layer.debug = true;
+        this.Player = this.map.objects.Player[0];
+        this.Demons = this.map.objects.Demons;
+
 
         // Player
-        this.p1 = this.game.add.sprite(64/2, this.game.world.height-193, 'player');
+        this.p1 = this.game.add.sprite(this.map.objects.Player[0].x, this.map.objects.Player[0].y, 'player');
         game.physics.arcade.enable(this.p1);
         game.camera.follow(this.p1, Phaser.Camera.FOLLOW_PLATFORMER);
         this.p1.body.setSize(25,50,0,0);
-        this.p1.body.gravity.y = 1000;
         this.p1.body.maxVelocity.setTo(200,9999);
         this.p1.body.drag.setTo(1000,-100);
         this.p1.anchor.setTo(0.5, 0.5);
         // this.p1.body.collideWorldBounds = true;
+
+        // Demons
+        this.demons = game.add.group();
+        this.map.createFromObjects('Demons', 1, 'demon', 0, true, false, this.demons);
+        this.demons.enableBody = true;
+        this.demons.physicsBodyType = Phaser.Physics.ARCADE;
+        this.demons.setAll('outOfBoundsKill', true);
+        this.demons.setAll('checkWorldBounds', true);
+        this.demons.callAll('animations.add', 'animations', 'demonwalk', [0, 1, 2, 3], 3, true);
+        game.physics.arcade.enable(this.demons);
+        this.demons.forEach(function (demon) {
+            demon.body.setSize(25,50, 0, 0);
+            demon.anchor.x = 0.5;
+        }, this, true);
 
         // Bullets
         this.bullets = game.add.group();
@@ -46,6 +63,7 @@ var mainState = {
         this.bullets.setAll('outOfBoundsKill', true);
         this.bullets.setAll('checkWorldBounds', true);
         this.bullets.setAll('body.velocity.x', 100);
+        this.bullets.setAll('body.gravity.y', -1000);
 
         // Controls
         this.cursor = this.game.input.keyboard.createCursorKeys();
@@ -57,7 +75,6 @@ var mainState = {
         this.p1.animations.add('walk',[1,2,3,4,5,6,7,8]);
         this.p1.animations.add('jump',[9]);
         this.p1.animations.add('crouch',[10]);
-        // this.p1.animations.add('walk',[0,1,2,3]);
 
         // Globals
         this.maxJumps = 2;
@@ -67,11 +84,19 @@ var mainState = {
     },
     update: function () {
         this.physics.arcade.collide(this.p1, this.layer);
+        game.physics.arcade.collide(this.demons, this.layer);
         game.physics.arcade.overlap(this.bullets, this.layer, this.bulletHitLayer, null, this);
+        game.physics.arcade.overlap(this.bullets, this.demons, this.bulletHitDemon, null, this);
+
+
         this.move();
+        this.demons.forEach(this.moveDemons, this, true);
     },
     render: function () {
         // this.game.debug.body(this.p1);
+        // this.demons.forEach(function (demon) {
+        //     this.game.debug.body(demon);
+        // }, this, true);
         // game.debug.text(game.time.fps +' fps' || '--', 2, 14, "#00ff00");
 
     },
@@ -127,6 +152,29 @@ var mainState = {
              this.p1.frame = stillFrame;
          }
     },
+    moveDemons: function (demon) {
+        var stopDistance = 30;
+        var viewDistance = 500;
+        if (this.p1.position.x < demon.position.x) {
+            // Player is left of demon
+            if ((demon.position.x - this.p1.position.x) < viewDistance) {
+                if ((demon.position.x - this.p1.position.x) > stopDistance) demon.body.velocity.x = -20;
+                else demon.body.velocity.x = 0;
+                demon.scale.x = 1;
+            } else demon.body.velocity.x = 0;
+
+        } else {
+            // Player is right of demon
+            if ((this.p1.position.x - demon.position.x) < viewDistance) {
+                if ((this.p1.position.x - demon.position.x) > stopDistance) demon.body.velocity.x = 20;
+                else demon.body.velocity.x = 0;
+                demon.scale.x = -1;
+            } else demon.body.velocity.x = 0;
+
+        }
+        if (demon.body.velocity.x !== 0) demon.play('demonwalk');
+        else demon.frame = 0;
+    },
     jump: function () {
         if (this.jumps < this.maxJumps) {
             this.p1.body.velocity.y = -350;
@@ -138,10 +186,8 @@ var mainState = {
         var bullet = this.bullets.getFirstExists(false);
         if (this.facingRight) {
             bullet.anchor.x = -1;
-            // bullet.scale.x = 1;
         } else if (this.facingLeft) {
             bullet.anchor.x = 1;
-            // bullet.scale.x = -1;
             vel = -500;
         } else {
 
@@ -152,9 +198,18 @@ var mainState = {
     },
     bulletHitLayer: function (bullet, layer) {
         bullet.frame = 1;
+        bullet.body.velocity.x = 0;
         setTimeout(function () {
             bullet.kill();
         }, 50);
+    },
+    bulletHitDemon: function (bullet, demon) {
+        bullet.frame = 1;
+        bullet.body.velocity.x = 0;
+        setTimeout(function () {
+            bullet.kill();
+        }, 50);
+        demon.body.velocity.x = 10;
     },
 };
 
